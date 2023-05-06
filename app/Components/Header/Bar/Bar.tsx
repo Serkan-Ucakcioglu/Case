8760d272ff3ba4a29de68ee9fdbae9ebc6c95e87
Serkan Uçakcıoğlu <serkanuckc123@gmail.com>
2023-05-06 17:43:13 +0300

    update

--- app/Components/Header/Bar/Bar.tsx
+++ app/Components/Header/Bar/Bar.tsx
@@ -1,21 +1,28 @@
+"use client";
+import React, { useState } from "react";
 import Calender from "@/app/assets/Calender";
 import Points from "@/app/assets/Points";
 import BarPopup from "./BarPopup";
 
 function Bar() {
+  const [show, setShow] = useState<Boolean | String>(false);
   return (
     <>
       <div className="w-full flex justify-between p-4 bg-[#F5F5F5] mt-6 px-12">
-        <div className="flex items-center ml-2">
+        <div
+          className="flex justify-center ml-2"
+          onMouseEnter={() => setShow(true)}
+          onMouseLeave={() => setShow(false)}
+        >
           <Points />
           <span className="ml-2 text-[#FF0D87]">Filtreler</span>
+          {show && <BarPopup setShow={setShow} />}
         </div>
         <div className="flex items-center">
           <Calender />
           <span className="ml-2">Takvimde Gör</span>
         </div>
       </div>
-      <BarPopup />
     </>
   );
 }
--- app/Components/Header/Bar/BarPopup.tsx
+++ app/Components/Header/Bar/BarPopup.tsx
@@ -1,5 +1,13 @@
-function BarPopup() {
-  return <div className="popup shadow-xl bg-white mt-2 ml-10 p-4">se </div>;
+function BarPopup({ setShow }: any) {
+  return (
+    <div
+      className="popup shadow-xl bg-white mt-2 ml-10 py-6 px-4"
+      onMouseEnter={() => setShow(true)}
+      onMouseLeave={() => setShow(false)}
+    >
+      se{" "}
+    </div>
+  );
 }
 
 export default BarPopup;
