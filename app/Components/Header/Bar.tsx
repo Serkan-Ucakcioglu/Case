4e671c15ae89b22b7ce6a13ff0a50385bb269b37
Serkan Uçakcıoğlu <serkanuckc123@gmail.com>
2023-05-06 15:57:27 +0300

    items-center add

--- app/Components/Header/Bar.tsx
+++ app/Components/Header/Bar.tsx
@@ -4,11 +4,11 @@ import Points from "@/app/assets/Points";
 function Bar() {
   return (
     <div className="w-full flex justify-between p-3 bg-[#F5F5F5] mt-6 px-8">
-      <div className="flex ml-2">
+      <div className="flex items-center ml-2">
         <Points />
         <span className="ml-2 text-[#FF0D87]">Filtreler</span>
       </div>
-      <div className="flex">
+      <div className="flex items-center">
         <Calender />
         <span className="ml-2">Takvimde Gör</span>
       </div>
