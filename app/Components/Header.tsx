8dc2e82a77fc81b6141f878fcdffc9438de3398f
Serkan Uçakcıoğlu <serkanuckc123@gmail.com>
2023-05-06 14:44:40 +0300

    class add

--- app/Components/Header.tsx
+++ app/Components/Header.tsx
@@ -1,4 +1,5 @@
 import Logo from "../assets/Logo";
+import Search from "../assets/Search";
 function Header() {
   return (
     <div className="flex flex-col">
@@ -6,7 +7,7 @@ function Header() {
         <Logo />
         <h1 className="head-title mt-[45px] font-bold">ETKİNLİKLER</h1>
       </div>
-      <div className="flex w-full mt-[40px]">
+      <div className="flex w-full items-center mt-[40px]">
         <ul className="flex gap-x-8">
           <li className="active">Tüm Etkinlikler</li>
           <li>Tiyatro</li>
@@ -15,7 +16,14 @@ function Header() {
           <li>Sinema</li>
           <li>Cocuk</li>
         </ul>
-        <div className=" flex ml-auto">se</div>
+        <div className="flex items-center border border-gray-200 py-3 p-1 focus:border-black hover:border-gray-400">
+          <Search />
+          <input
+            type="text"
+            placeholder="Etkinlik Ara"
+            className="outline-none ml-1"
+          />
+        </div>
       </div>
     </div>
   );
---
+++ app/assets/Search.tsx
@@ -0,0 +1,21 @@
+function Search() {
+  return (
+    <>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width={24}
+        height={24}
+        fill="none"
+      >
+        <path
+          fill="#000"
+          fillRule="evenodd"
+          d="M9.5 16a6.5 6.5 0 1 0 0-13 6.5 6.5 0 0 0 0 13Zm5.793.707 5 5 1.414-1.414-5-5-1.414 1.414Z"
+          clipRule="evenodd"
+        />
+      </svg>
+    </>
+  );
+}
+
+export default Search;
