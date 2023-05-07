a23b1e3661780ea8b6cbefaf74f6bbf152f13332
Serkan Uçakcıoğlu <serkanuckc123@gmail.com>
2023-05-07 16:10:25 +0300

    update

--- app/Components/Header/Main/Main.tsx
+++ app/Components/Header/Main/Main.tsx
@@ -4,7 +4,7 @@ function Main() {
   return (
     <>
       <div className="flex relative  justify-center px-4 items-center mt-[40px]">
-        <ul className="flex gap-x-4 sm:gap-x-8">
+        <ul className="flex gap-x-3 sm:gap-x-8">
           <li className="active">Tüm Etkinlikler</li>
           <li className="focus:active">Tiyatro</li>
           <li>Konser</li>
