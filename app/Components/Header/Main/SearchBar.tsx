364cd7609bab354a498af55c853797d234c05a03
Serkan Uçakcıoğlu <serkanuckc123@gmail.com>
2023-05-07 16:11:56 +0300

    update

--- app/Components/Header/Main/SearchBar.tsx
+++ app/Components/Header/Main/SearchBar.tsx
@@ -2,7 +2,7 @@ import Search from "../../../assets/Search";
 function SearchBar() {
   return (
     <>
-      <div className="hidden hover:shadow sm:flex items-center absolute right-60 max-w-[200px] border border-gray-200 py-3 px-2 focus:border-black hover:border-gray-400">
+      <div className="hidden hover:shadow md:flex items-center absolute right-60 max-w-[200px] border border-gray-200 py-3 px-2 focus:border-black hover:border-gray-400">
         <Search />
         <input
           type="text"
