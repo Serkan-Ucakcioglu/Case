0b11a9c47bf038005a9acc77679c647ac088221e
Serkan Uçakcıoğlu <serkanuckc123@gmail.com>
2023-05-07 12:49:43 +0300

    arr

--- app/Components/Header/Bar/BarPopup.tsx
+++ app/Components/Header/Bar/BarPopup.tsx
@@ -5,6 +5,13 @@ type Bar = {
 };
 
 function BarPopup({ setShow }: Bar) {
+  const arr = [
+    { name: "Maxium Uniq Box" },
+    { name: "Maximum Uniq Lounge" },
+    { name: "Maximum Uniq Açıkhava" },
+    { name: "Bahçe Fuaye" },
+  ];
+
   return (
     <div
       className="popup shadow-xl bg-white mt-2 ml-10 py-6 px-4"
@@ -15,8 +22,16 @@ function BarPopup({ setShow }: Bar) {
         <div className="bg-[#FF0D87] h-5 w-5 flex justify-center items-center">
           <Selected />
         </div>
-        <label htmlFor="ch">Maximum Uniq Hall</label>
+        <span className="text-[#000000]">Maximum Uniq Hall</span>
       </div>
+      {arr?.map((obj) => {
+        return (
+          <div className="flex mt-2 gap-2 items-center">
+            <div className="h-5 w-5 border border-gray-200"></div>
+            <span className="text-[#000000]">{obj?.name}</span>
+          </div>
+        );
+      })}
     </div>
   );
 }
