f48437e1cedb401903959309a58c78741bd2fdea
Serkan Uçakcıoğlu <serkanuckc123@gmail.com>
2023-05-07 15:26:46 +0300

    update

--- app/data/data.ts
+++ app/data/data.ts
@@ -9,6 +9,7 @@ type Arrays = {
   date: String;
   tag: String;
   title: String;
+  color: String;
 };
 
 export const arr: Arrays[] = [
@@ -17,35 +18,41 @@ export const arr: Arrays[] = [
     date: "8 Eylül Çarşamba 21:15",
     tag: "TİYATRO",
     title: "BKM Yaz Etkinlikleri:Zengin Mutfağı",
+    color: "#B77CB8",
   },
   {
     img: img1,
     date: "Yasemin Sakallıoğlu",
     tag: "STANDUP",
     title: "BKM Yaz Etkinlikleri:Zengin Mutfağı",
+    color: "#F19653",
   },
   {
     img: img2,
     date: "8 Eylül Çarşamba 21:15",
     tag: "SİNEMA",
     title: "Sinema Keyfi: Rifkin’s Festival",
+    color: "#F07266",
   },
   {
     img: img4,
     date: "8 Eylül Çarşamba 21:15",
     tag: "KONSER",
     title: "BKM Yaz Etkinlikleri:Zengin Mutfağı",
+    color: "9FAE5D",
   },
   {
     img: img1,
     date: "8 Eylül Çarşamba 21:15",
     tag: "STANDUP",
     title: "Yasemin Sakallıoğlu",
+    color: "#F19653",
   },
   {
     img: img3,
     date: "8 Eylül Çarşamba 21:15",
     tag: "SİNEMA",
     title: "Sinema Keyfi: Rifkin’s Festival",
+    color: "#F07266",
   },
 ];
