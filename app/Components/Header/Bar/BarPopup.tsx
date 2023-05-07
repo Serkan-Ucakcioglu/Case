be2063f2680f5ba929830849d09de1e9df282a56
Serkan Uçakcıoğlu <serkanuckc123@gmail.com>
2023-05-07 13:00:20 +0300

    update

--- app/Components/Header/Bar/BarPopup.tsx
+++ app/Components/Header/Bar/BarPopup.tsx
@@ -18,21 +18,32 @@ function BarPopup({ setShow }: Bar) {
       onMouseEnter={() => setShow(true)}
       onMouseLeave={() => setShow(false)}
     >
-      <h1 className="font-bold mb-3">Etkinlik Alanı</h1>
-      <div className="flex gap-3 items-center border-b p-1 border-gray-100">
-        <div className="bg-[#FF0D87] h-5 w-5 flex justify-center items-center">
-          <Selected />
-        </div>
-        <span className="text-[#000000]">Maximum Uniq Hall</span>
-      </div>
-      {arr?.map((obj) => {
-        return (
-          <div className="flex mt-2 p-1 gap-3 border-b last:border-none border-gray-100 items-center">
-            <div className="h-5 w-5 border border-gray-200"></div>
-            <span className="text-[#000000]">{obj?.name}</span>
+      <>
+        <h1 className="font-bold mb-3">Etkinlik Alanı</h1>
+        <div className="flex gap-3 items-center border-b p-1 border-gray-100">
+          <div className="bg-[#FF0D87] h-5 w-5 flex justify-center items-center">
+            <Selected />
           </div>
-        );
-      })}
+          <span className="text-[#000000]">Maximum Uniq Hall</span>
+        </div>
+        {arr?.map((obj) => {
+          return (
+            <div className="flex mt-2 p-1 gap-3 border-b last:border-none border-gray-100 items-center">
+              <div className="h-5 w-5 border border-gray-200"></div>
+              <span className="text-[#000000]">{obj?.name}</span>
+            </div>
+          );
+        })}
+      </>
+      <h1 className="font-bold mt-8">Etkinlik Tarihi</h1>
+      <div className="flex mt-2 p-1 gap-3 border-b last:border-none border-gray-100 items-center">
+        <div className="h-5 w-5 border border-gray-200"></div>
+        <span className="text-[#000000]">Güncel Etkinlikler</span>
+      </div>
+      <div className="flex mt-2 p-1 gap-3 border-b last:border-none border-gray-100 items-center">
+        <div className="h-5 w-5 border border-gray-200"></div>
+        <span className="text-[#000000]">Geçmiş etkinlikler</span>
+      </div>
     </div>
   );
 }
